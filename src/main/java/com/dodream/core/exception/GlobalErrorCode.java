aeebfba9beaada60164b722850d75df275b75a42
chyun7114 <chyun7114@naver.com>
2025-04-12 22:59:36 +0900

    feat: jpa 설정 및 BaseLongIdEntity 생성

--- src/main/java/com/dodream/core/exception/GlobalErrorCode.java
+++ src/main/java/com/dodream/core/exception/GlobalErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum GlobalErrorCode implements BaseErrorCode<RuntimeException> {
-
+    GET_DATA_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB 연결은 되었지만 데이터를 가져오지 못했습니다."),
+    DB_CONNECTION_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB 연결에 실패했습니다."),
     ALREADY_PROCESS_STARTED(HttpStatus.BAD_REQUEST, "이미 처리중인 요청입니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 내부 오류입니다.");
 
