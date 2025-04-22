366c346c9eb30a582d04f9ed4cabb3a8352c00e6
chyun7114 <chyun7114@naver.com>
2025-04-22 15:51:17 +0900

    feat: 중분류 지역코드 받아오기

--- src/main/java/com/dodream/region/application/RegionService.java
+++ src/main/java/com/dodream/region/application/RegionService.java
@@ -1,33 +1,76 @@
 package com.dodream.region.application;
 
+import com.dodream.region.dto.response.RegionResponse;
+import com.dodream.region.exception.RegionErrorCode;
 import com.dodream.region.infrastructure.RegionApiProperties;
 import com.dodream.region.infrastructure.RegionFeignClient;
 import com.dodream.core.infrastructure.converter.XmlToJsonConverter;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class RegionService {
 
     private final RegionFeignClient regionFeignClient;
     private final RegionApiProperties regionApiProperties;
     private final XmlToJsonConverter xmlToJsonConverter;
+    private final ObjectMapper objectMapper;
 
-    public String getRegionCode(
-            String searchType1,
-            String searchOption1,
-            String searchOption2
-    ) {
+    public RegionResponse.SrchList getLargeRegionCode() {
         String regionXML = regionFeignClient.getRegionCode(
                 regionApiProperties.getApiKey(),
                 "XML",
                 "1",
-                searchType1,
-                searchOption1,
-                searchOption2
+                "00",
+                null,
+                null
         );
 
-        return xmlToJsonConverter.convertXmlToJson(regionXML);
+        String regionJSON = xmlToJsonConverter.convertXmlToJson(regionXML);
+
+        try {
+            RegionResponse response = objectMapper.readValue(regionJSON, RegionResponse.class);
+            return response.srchList();
+        } catch (JsonProcessingException e) {
+            throw RegionErrorCode.JSON_TO_OBJECT_CONVERT_ERROR.toException();
+        }
+    }
+
+    public List<RegionResponse.ScnItem> getMiddleRegion() {
+        List<RegionResponse.ScnItem> largeRegions = getLargeRegionCode().scnList();
+        List<RegionResponse.ScnItem> result = new ArrayList<>();
+
+        for(RegionResponse.ScnItem largeRegionItem : largeRegions){
+            String largeCode = largeRegionItem.rsltCode();
+
+            String middleXML = regionFeignClient.getRegionCode(
+                    regionApiProperties.getApiKey(),
+                    "XML",
+                    "1",
+                    "01",
+                    largeCode,
+                    null
+            );
+
+            String regionJSON = xmlToJsonConverter.convertXmlToJson(middleXML);
+
+            try {
+                RegionResponse response = objectMapper.readValue(regionJSON, RegionResponse.class);
+                List<RegionResponse.ScnItem> scnList = response.srchList().scnList();
+                result.addAll(scnList);
+            } catch (JsonProcessingException e) {
+                throw RegionErrorCode.JSON_TO_OBJECT_CONVERT_ERROR.toException();
+            }
+        }
+
+        return result;
     }
 }
--- src/main/java/com/dodream/region/controller/RegionController.java
+++ src/main/java/com/dodream/region/controller/RegionController.java
@@ -2,12 +2,14 @@ package com.dodream.region.controller;
 
 import com.dodream.core.presentation.RestResponse;
 import com.dodream.region.application.RegionService;
+import com.dodream.region.dto.response.RegionResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -17,13 +19,15 @@ public class RegionController {
 
     private final RegionService regionService;
 
-    @GetMapping
-    public ResponseEntity<RestResponse<String>> getRegions(
-            @RequestParam(name = "searchType") String searchType,
-            @RequestParam(name = "searchOption1", required = false) String searchOption1,
-            @RequestParam(name = "searchOption2", required = false) String searchOption2
-    ){
-        String result = regionService.getRegionCode(searchType, searchOption1, searchOption2);
+    @GetMapping("/large")
+    public ResponseEntity<RestResponse<RegionResponse.SrchList>> getLargeRegions(){
+        RegionResponse.SrchList result = regionService.getLargeRegionCode();
+        return ResponseEntity.ok(new RestResponse<>(result));
+    }
+
+    @GetMapping("/middle")
+    public ResponseEntity<RestResponse<List<RegionResponse.ScnItem>>> getMiddleRegions(){
+        List<RegionResponse.ScnItem> result = regionService.getMiddleRegion();
         return ResponseEntity.ok(new RestResponse<>(result));
     }
 }
