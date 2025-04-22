24b9e0609a675c426f937b9548ada5a652942521
chyun7114 <chyun7114@naver.com>
2025-04-22 18:59:56 +0900

    feat: 서버 최초 실행시 지역 코드 data init 되도록 설정

---
+++ src/main/java/com/dodream/region/domain/Region.java
@@ -0,0 +1,37 @@
+package com.dodream.region.domain;
+
+import com.dodream.core.infrastructure.jpa.entity.BaseLongIdEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@SuperBuilder
+@Table(name = "region")
+public class Region extends BaseLongIdEntity {
+
+    @Column(name = "region_code", unique = true, nullable = false)
+    private String regionCode;
+
+    @Column(name = "region_name", nullable = false)
+    private String regionName;
+
+    public static Region create(
+            String regionCode,
+            String regionName
+    ){
+        return Region.builder()
+                .regionCode(regionCode)
+                .regionName(regionName)
+                .build();
+    }
+}
