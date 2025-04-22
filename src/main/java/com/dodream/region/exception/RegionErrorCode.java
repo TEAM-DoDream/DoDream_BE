b8c4c861bb6ad3ba50658a554ee52087d39da951
chyun7114 <chyun7114@naver.com>
2025-04-22 15:51:17 +0900

    feat: 중분류 지역코드 받아오기

--- src/main/java/com/dodream/region/exception/RegionErrorCode.java
+++ src/main/java/com/dodream/region/exception/RegionErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum RegionErrorCode implements BaseErrorCode<DomainException> {
-    ;
+    JSON_TO_OBJECT_CONVERT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "객체 변환에 실패했습니다.");
 
     private final HttpStatus httpStatus;
 
