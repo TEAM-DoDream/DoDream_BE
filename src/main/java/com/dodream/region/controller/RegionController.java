7ff3361daf2db628388a734047e93bae93f90cf2
chyun7114 <chyun7114@naver.com>
2025-04-22 15:53:50 +0900

    chore: 서브모듈 커밋 반영

---
+++ src/main/java/com/dodream/region/controller/RegionController.java
@@ -0,0 +1,28 @@
+package com.dodream.region.controller;
+
+import com.dodream.core.presentation.RestResponse;
+import com.dodream.region.application.RegionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/region")
+@RequiredArgsConstructor
+public class RegionController {
+
+    private final RegionService regionService;
+
+    @GetMapping
+    public ResponseEntity<RestResponse<String>> getRegions(
+            @RequestParam(name = "searchType") String searchType,
+            @RequestParam(name = "searchOption1", required = false) String searchOption1,
+            @RequestParam(name = "searchOption2", required = false) String searchOption2
+    ) {
+        String result = regionService.getRegionCode(searchType, searchOption1, searchOption2);
+        return ResponseEntity.ok(new RestResponse<>(result));
+    }
+}
