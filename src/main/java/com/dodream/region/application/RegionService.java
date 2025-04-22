30177bbeaf8abbe2467e9679ff473c88ab04b4e8
chyun7114 <chyun7114@naver.com>
2025-04-22 19:21:53 +0900

    feat: 지역 정보 검색 관련 컨트롤러 및 서비스 완성

---
+++ src/main/java/com/dodream/region/application/RegionService.java
@@ -0,0 +1,40 @@
+package com.dodream.region.application;
+
+import com.dodream.region.domain.Region;
+import com.dodream.region.dto.response.RegionResponseDto;
+import com.dodream.region.exception.RegionErrorCode;
+import com.dodream.region.repository.RegionRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class RegionService {
+
+    private final RegionRepository regionRepository;
+
+    public List<RegionResponseDto> findAllRegions(){
+        return regionRepository.findAll()
+                .stream()
+                .map(RegionResponseDto::from)
+                .toList();
+    }
+
+    public RegionResponseDto findByName(String name){
+        Region region = regionRepository.findByRegionName(name)
+                .orElseThrow(RegionErrorCode.NOT_FOUND_REGION::toException);
+
+        return RegionResponseDto.from(region);
+    }
+
+    public RegionResponseDto findByRegionCode(String code){
+        Region region = regionRepository.findByRegionCode(code)
+                .orElseThrow(RegionErrorCode.NOT_FOUND_REGION::toException);
+
+        return RegionResponseDto.from(region);
+    }
+}
