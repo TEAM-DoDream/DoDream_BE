3db211e3aad3bf5e7be9d95c1ae9b3d074282cf2
chyun7114 <chyun7114@naver.com>
2025-04-22 18:59:56 +0900

    feat: 서버 최초 실행시 지역 코드 data init 되도록 설정

---
+++ src/main/java/com/dodream/region/infrastructure/RegionInitializer.java
@@ -0,0 +1,43 @@
+package com.dodream.region.infrastructure;
+
+import com.dodream.common.dto.response.CommonResponse;
+import com.dodream.region.application.RegionApiService;
+import com.dodream.region.domain.Region;
+import com.dodream.region.repository.RegionRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Log4j2
+public class RegionInitializer {
+
+    private final RegionRepository regionRepository;
+    private final RegionApiService regionApiService;
+
+    @PostConstruct
+    public void initRegionData() {
+        log.info("[initRegionData] - 지역 코드 초기화 시작");
+
+        if(regionRepository.count() != 0) {
+            // 이미 데이터 init된 경우라면 수행 x
+            return;
+        }
+
+        List<CommonResponse.ScnItem> regionList = regionApiService.getMiddleRegion();
+
+        List<Region> regionJpaEntityList = regionList.stream()
+                .map(item -> Region.create(
+                        item.rsltCode(),
+                        item.rsltName()
+                ))
+                .toList();
+
+        regionRepository.saveAll(regionJpaEntityList);
+        log.info("[initRegionData] - 지역 코드 초기화 완료 {}개 저장됨.", regionJpaEntityList.size());
+    }
+}
