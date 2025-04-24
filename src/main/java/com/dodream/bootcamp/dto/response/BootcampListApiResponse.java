e6185d4f135f37a588f6a06565812d14a2f2ac2c
chyun7114 <chyun7114@naver.com>
2025-04-24 20:53:27 +0900

    feat: 응답 dto값 변경

--- src/main/java/com/dodream/bootcamp/dto/response/BootcampListApiResponse.java
+++ src/main/java/com/dodream/bootcamp/dto/response/BootcampListApiResponse.java
@@ -18,89 +18,44 @@ public record BootcampListApiResponse(
         List<BootcampItem> srchList
 ) {
     public record BootcampItem(
-            @JsonProperty("eiEmplCnt3Gt10")
-            String eiEmplCnt3Gt10,
-
-            @JsonProperty("eiEmplRate6")
-            String eiEmplRate6,
-
-            @JsonProperty("eiEmplCnt3")
-            String eiEmplCnt3,
-
-            @JsonProperty("eiEmplRate3")
-            String eiEmplRate3,
-
-            @JsonProperty("title")
-            String title,
-
-            @JsonProperty("realMan")
-            String realMan,
-
-            @JsonProperty("telNo")
-            String telNo,
-
-            @JsonProperty("stdgScor")
-            String stdgScor,
-
-            @JsonProperty("traStartDate")
-            String traStartDate,
-
-            @JsonProperty("grade")
-            String grade,
-
-            @JsonProperty("ncsCd")
-            String ncsCd,
-
-            @JsonProperty("regCourseMan")
-            String regCourseMan,
-
-            @JsonProperty("trprDegr")
-            String trprDegr,
-
             @JsonProperty("address")
             String address,
-
-            @JsonProperty("traEndDate")
-            String traEndDate,
-
-            @JsonProperty("subTitle")
-            String subTitle,
-
-            @JsonProperty("instCd")
-            String instCd,
-
-            @JsonProperty("trngAreaCd")
-            String trngAreaCd,
-
-            @JsonProperty("trprId")
-            String trprId,
-
-            @JsonProperty("yardMan")
-            String yardMan,
 
             @JsonProperty("courseMan")
             String courseMan,
 
-            @JsonProperty("trainTarget")
-            String trainTarget,
+            @JsonProperty("eiEmplRate3")
+            String eiEmplRate3,
 
-            @JsonProperty("trainTargetCd")
-            String trainTargetCd,
+            @JsonProperty("realMan")
+            String realMan,
 
-            @JsonProperty("trainstCstId")
-            String trainstCstId,
+            @JsonProperty("subTitle")
+            String subTitle,
 
-            @JsonProperty("contents")
-            String contents,
-
-            @JsonProperty("subTitleLink")
-            String subTitleLink,
+            @JsonProperty("title")
+            String title,
 
             @JsonProperty("titleLink")
             String titleLink,
 
-            @JsonProperty("titleIcon")
-            String titleIcon
+            @JsonProperty("traEndDate")
+            String traEndDate,
+
+            @JsonProperty("traStartDate")
+            String traStartDate,
+
+            @JsonProperty("trainstCstId")
+            String trainstCstId,
+
+            @JsonProperty("trngAreaCd")
+            String trngAreaCd,
+
+            @JsonProperty("trprDegr")
+            String trprDegr,
+
+            @JsonProperty("trprId")
+            String trprId
     ) {}
 }
 
