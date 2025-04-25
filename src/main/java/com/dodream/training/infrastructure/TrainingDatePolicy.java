d0ff32af213a134d9d5b13ca6efed8d5b3433da6
chyun7114 <chyun7114@naver.com>
2025-04-25 19:29:58 +0900

    fix: 시작 종료일 수정

--- src/main/java/com/dodream/training/infrastructure/TrainingDatePolicy.java
+++ src/main/java/com/dodream/training/infrastructure/TrainingDatePolicy.java
@@ -1,6 +1,5 @@
 package com.dodream.training.infrastructure;
 
-import jakarta.annotation.PostConstruct;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
