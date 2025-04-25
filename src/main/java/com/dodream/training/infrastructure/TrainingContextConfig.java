8b949cd7e7a1dcd1356d31b273f6cafeb2ba332a
chyun7114 <chyun7114@naver.com>
2025-04-25 22:27:56 +0900

    feat: 일학습병행관련 컨트롤러 및 서비스 로직 구현 완료

--- src/main/java/com/dodream/training/infrastructure/TrainingContextConfig.java
+++ src/main/java/com/dodream/training/infrastructure/TrainingContextConfig.java
@@ -15,26 +15,15 @@ public class TrainingContextConfig {
 
     private final BootCampApiCaller bootCampApiCaller;
     private final DualTrainingApiCaller dualTrainingApiCaller;
-<<<<<<< HEAD
     private final TrainingDatePolicy trainingDatePolicy;
-    @Bean
-    public TrainingApiExecuter bootcampApiExecuter() {
-        return new BootcampApiExecuter(bootCampApiCaller, trainingDatePolicy);
-=======
 
     @Bean
     public TrainingApiExecuter bootcampApiExecuter() {
-        return new BootcampApiExecuter(bootCampApiCaller);
->>>>>>> 4d30e0d (feat: 일학습병행 훈련과정 인프라 로직 작성 및 인터페이스로 책임 분리)
+        return new BootcampApiExecuter(bootCampApiCaller, trainingDatePolicy);
     }
 
     @Bean
     public TrainingApiExecuter dualTrainingApiExecuter() {
-<<<<<<< HEAD
         return new DualTrainingApiExecuter(dualTrainingApiCaller, trainingDatePolicy);
-=======
-        return new DualTrainingApiExecuter(dualTrainingApiCaller);
->>>>>>> 4d30e0d (feat: 일학습병행 훈련과정 인프라 로직 작성 및 인터페이스로 책임 분리)
     }
-
 }
--- src/main/java/com/dodream/training/util/executer/BootcampApiExecuter.java
+++ src/main/java/com/dodream/training/util/executer/BootcampApiExecuter.java
@@ -12,14 +12,20 @@ public class BootcampApiExecuter implements TrainingApiExecuter {
     private final TrainingApiCaller trainingApiCaller;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     private final TrainingDatePolicy trainingDatePolicy;
 
 =======
 >>>>>>> 4d30e0d (feat: 일학습병행 훈련과정 인프라 로직 작성 및 인터페이스로 책임 분리)
+=======
+    private final TrainingDatePolicy trainingDatePolicy;
+
+>>>>>>> 8b81609 (feat: 일학습병행관련 컨트롤러 및 서비스 로직 구현 완료)
     @Override
     public String callListApi(
             String pageNum, String regionCode, String ncsCode
     ) {
+<<<<<<< HEAD
 <<<<<<< HEAD
         String startDate = trainingDatePolicy.calculateStartDate();
         String endDate = trainingDatePolicy.calculateEndDate();
@@ -27,6 +33,10 @@ public class BootcampApiExecuter implements TrainingApiExecuter {
         String startDate = TrainingDatePolicy.calculateStartDate();
         String endDate = TrainingDatePolicy.calculateEndDate();
 >>>>>>> 4d30e0d (feat: 일학습병행 훈련과정 인프라 로직 작성 및 인터페이스로 책임 분리)
+=======
+        String startDate = trainingDatePolicy.calculateStartDate();
+        String endDate = trainingDatePolicy.calculateEndDate();
+>>>>>>> 8b81609 (feat: 일학습병행관련 컨트롤러 및 서비스 로직 구현 완료)
 
         return trainingApiCaller.getListApi(
                 pageNum, regionCode, ncsCode, startDate, endDate
