e949baafec4a8e66fa2b341e418a7e2d5a9e4e78
chyun7114 <chyun7114@naver.com>
2025-04-25 21:56:03 +0900

    feat: 일학습병행 훈련과정 인프라 로직 작성 및 인터페이스로 책임 분리

--- src/main/java/com/dodream/training/util/executer/BootcampApiExecuter.java
+++ src/main/java/com/dodream/training/util/executer/BootcampApiExecuter.java
@@ -11,14 +11,22 @@ public class BootcampApiExecuter implements TrainingApiExecuter {
     @Qualifier("bootCampApiCaller")
     private final TrainingApiCaller trainingApiCaller;
 
+<<<<<<< HEAD
     private final TrainingDatePolicy trainingDatePolicy;
 
+=======
+>>>>>>> 4d30e0d (feat: 일학습병행 훈련과정 인프라 로직 작성 및 인터페이스로 책임 분리)
     @Override
     public String callListApi(
             String pageNum, String regionCode, String ncsCode
     ) {
+<<<<<<< HEAD
         String startDate = trainingDatePolicy.calculateStartDate();
         String endDate = trainingDatePolicy.calculateEndDate();
+=======
+        String startDate = TrainingDatePolicy.calculateStartDate();
+        String endDate = TrainingDatePolicy.calculateEndDate();
+>>>>>>> 4d30e0d (feat: 일학습병행 훈련과정 인프라 로직 작성 및 인터페이스로 책임 분리)
 
         return trainingApiCaller.getListApi(
                 pageNum, regionCode, ncsCode, startDate, endDate
