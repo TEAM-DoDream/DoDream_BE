9e04818e5a327e48e82c2ece47384e122c3dbecc
chyun7114 <chyun7114@naver.com>
2025-04-25 19:02:16 +0900

    feat: TrainingAPiExecuter로 코드 분리

--- src/main/java/com/dodream/training/application/TrainingService.java
+++ src/main/java/com/dodream/training/application/TrainingService.java
@@ -2,9 +2,8 @@ package com.dodream.training.application;
 
 import com.dodream.training.dto.response.BootcampDetailApiResponse;
 import com.dodream.training.dto.response.BootcampListApiResponse;
-import com.dodream.training.infrastructure.TrainingApiCaller;
-import com.dodream.training.infrastructure.TrainingDatePolicy;
 import com.dodream.training.infrastructure.mapper.BootcampMapper;
+import com.dodream.training.util.TrainingApiExecuter;
 import com.dodream.training.util.TrainingCodeResolver;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -15,21 +14,18 @@ import org.springframework.stereotype.Service;
 @Log4j2
 public class TrainingService {
 
-    private final TrainingApiCaller trainingApiCaller;
     private final BootcampMapper bootcampMapper;
     private final TrainingCodeResolver trainingCodeResolver;
+    private final TrainingApiExecuter trainingApiExecuter;
 
     public BootcampListApiResponse getList(
             String pageNum, String regionName, String ncsName
     ) {
-        String startDate = TrainingDatePolicy.calculateStartDate();
-        String endDate = TrainingDatePolicy.calculateEndDate();
-
         String regionCode = trainingCodeResolver.resolveRegionCode(regionName);
         String ncsCode = trainingCodeResolver.resolveNcsCode(ncsName);
 
-        String result = trainingApiCaller.bootcampListApiCaller(
-                pageNum, regionCode, ncsCode, startDate, endDate
+        String result = trainingApiExecuter.callBootcampListApi(
+                pageNum, regionCode, ncsCode
         );
 
         return bootcampMapper.jsonStringToBootcampListApiResponse(result);
@@ -38,7 +34,7 @@ public class TrainingService {
     public BootcampDetailApiResponse getDetail(
             String srchTrprId, String srchTrprDegr, String srchTorgId
     ){
-        String result = trainingApiCaller.bootcampDetailApiCaller(
+        String result = trainingApiExecuter.callBootcampDetailApi(
                 srchTrprId, srchTrprDegr, srchTorgId
         );
 
