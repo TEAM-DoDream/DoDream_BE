bce699bd2a5d9df43c790052b360b73f708df7b3
chyun7114 <chyun7114@naver.com>
2025-04-25 18:38:13 +0900

    fix: detailResponseDto 수정

--- src/main/java/com/dodream/bootcamp/dto/response/BootcampDetailApiResponse.java
+++ src/main/java/com/dodream/bootcamp/dto/response/BootcampDetailApiResponse.java
@@ -3,14 +3,37 @@ package com.dodream.bootcamp.dto.response;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record BootcampDetailApiResponse(
-        @JsonProperty("trprDegr") int trprDegr,
-        @JsonProperty("trprNm") String trprNm,
-        @JsonProperty("inoNm") String inoNm,
-        @JsonProperty("ncsCd") String ncsCd,
-        @JsonProperty("ncsNm") String ncsNm,
-        @JsonProperty("trDcnt") int trDcnt,
-        @JsonProperty("trtm") int trtm,
-        @JsonProperty("torgParGrad") String torgParGrad,
-        @JsonProperty("instPerTrco") int instPerTrco
+
+        @JsonProperty("inst_base_info")
+        InstBaseInfo instBaseInfo
 ) {
+    public record InstBaseInfo(
+
+            @JsonProperty("trprDegr")
+            int trprDegr,
+
+            @JsonProperty("trprNm")
+            String trprNm,
+
+            @JsonProperty("inoNm")
+            String inoNm,
+
+            @JsonProperty("ncsCd")
+            String ncsCd,
+
+            @JsonProperty("ncsNm")
+            String ncsNm,
+
+            @JsonProperty("trDcnt")
+            int trDcnt,
+
+            @JsonProperty("trtm")
+            int trtm,
+
+            @JsonProperty("torgParGrad")
+            String torgParGrad,
+
+            @JsonProperty("instPerTrco")
+            int instPerTrco
+    ){}
 }
