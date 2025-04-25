1c389dc1661be52488dce54c55638a36317b5eef
sseongeun <96720963+sseongeun@users.noreply.github.com>
2025-04-25 22:44:59 +0900

    feat : MemberLoginRequestDto - schema추가 (#51)

--- src/main/java/com/dodream/member/dto/request/MemberLoginRequestDto.java
+++ src/main/java/com/dodream/member/dto/request/MemberLoginRequestDto.java
@@ -1,7 +1,11 @@
 package com.dodream.member.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record MemberLoginRequestDto(
+    @Schema(description = "아이디", example = "dodream")
     String memberId,
+    @Schema(description = "비밀번호", example = "hello2025")
     String password
 ){
 }
