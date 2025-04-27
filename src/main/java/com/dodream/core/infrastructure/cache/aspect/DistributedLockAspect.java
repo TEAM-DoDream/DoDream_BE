3f225956957d5d36db65b66f000ede1c928d7122
chyun7114 <chyun7114@naver.com>
2025-04-27 18:08:40 +0900

    feat: 분산락 어노테이션 구현

---
+++ src/main/java/com/dodream/core/infrastructure/cache/aspect/DistributedLockAspect.java
@@ -0,0 +1,44 @@
+package com.dodream.core.infrastructure.cache.aspect;
+
+import com.dodream.core.exception.GlobalErrorCode;
+import com.dodream.core.infrastructure.cache.annotation.DistributedLock;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+@Log4j2
+public class DistributedLockAspect {
+
+    private final RedissonClient redissonClient;
+
+    @Around("@annotation(distributedLock)")
+    public Object around(ProceedingJoinPoint joinPoint, DistributedLock distributedLock) throws Throwable {
+        String lockKey = distributedLock.lockName();
+        RLock lock = redissonClient.getLock(lockKey);
+        boolean available = lock.tryLock(distributedLock.waitTime(), distributedLock.leaseTime(), TimeUnit.SECONDS);
+
+        if (!available) {
+            throw GlobalErrorCode.CANNOT_GET_CACHE_NAME.toException();
+        }
+
+        try {
+            return joinPoint.proceed();
+        } finally {
+            if (lock.isHeldByCurrentThread()) {
+                lock.unlock();
+            }
+        }
+    }
+}
