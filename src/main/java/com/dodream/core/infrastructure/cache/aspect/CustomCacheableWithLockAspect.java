789e27ee3691268b472a99085764383796402e7a
chyun7114 <chyun7114@naver.com>
2025-04-27 13:41:08 +0900

    refactor: BaseCacheAbstract로 리팩토링

--- src/main/java/com/dodream/core/infrastructure/cache/aspect/CustomCacheableWithLockAspect.java
+++ src/main/java/com/dodream/core/infrastructure/cache/aspect/CustomCacheableWithLockAspect.java
@@ -1,8 +1,8 @@
 package com.dodream.core.infrastructure.cache.aspect;
 
 import com.dodream.core.exception.GlobalErrorCode;
+import com.dodream.core.infrastructure.cache.annotation.CustomCacheable;
 import com.dodream.core.infrastructure.cache.annotation.CustomCacheableWithLock;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -14,29 +14,28 @@ import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
-import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 @Aspect
-@RequiredArgsConstructor
 @Log4j2
 @Component
-public class CustomCacheableWithLockAspect {
+public class CustomCacheableWithLockAspect extends BaseCacheAbstract{
 
-    private final RedisTemplate<String, Object> redisTemplate;
     private final RedissonClient redissonClient;
 
+    public CustomCacheableWithLockAspect(RedisTemplate<String, Object> redisTemplate, RedissonClient redissonClient) {
+        super(redisTemplate);
+        this.redissonClient = redissonClient;
+    }
+
     @Around("@annotation(customCacheableWithLock)")
-    public Object around(
-            ProceedingJoinPoint joinPoint, CustomCacheableWithLock customCacheableWithLock
-    ) throws Throwable {
+    public Object around(ProceedingJoinPoint joinPoint, CustomCacheableWithLock customCacheableWithLock) throws Throwable {
         String cacheKey = generateCacheKey(joinPoint, customCacheableWithLock);
 
         ValueOperations<String, Object> operations = redisTemplate.opsForValue();
         Object cacheValue = operations.get(cacheKey);
 
-        if(cacheValue != null){
+        if (cacheValue != null) {
             log.info("[{}] 레디스 캐시 저장 확인 - cacheKey=({})",
                     joinPoint.getSignature().getDeclaringTypeName(), cacheKey);
             return cacheValue;
@@ -45,18 +44,15 @@ public class CustomCacheableWithLockAspect {
         String lockKey = "lock:" + cacheKey;
         RLock lock = redissonClient.getLock(lockKey);
 
-        try{
-            boolean isLocked = lock.tryLock(5,10, TimeUnit.SECONDS);
-            if(!isLocked){
-                log.warn(
-                        "[{}] 락 획득 실패!! - Lock Key : {}",
-                        joinPoint.getSignature().getDeclaringTypeName(),
-                        lockKey
-                );
+        try {
+            boolean isLocked = lock.tryLock(5, 10, TimeUnit.SECONDS);
+            if (!isLocked) {
+                log.warn("[{}] 락 획득 실패!! - Lock Key : {}",
+                        joinPoint.getSignature().getDeclaringTypeName(), lockKey);
             }
 
             cacheValue = operations.get(cacheKey);
-            if(cacheValue != null){
+            if (cacheValue != null) {
                 log.info("[{}] 레디스 캐시 저장 확인 - cacheKey=({})",
                         joinPoint.getSignature().getDeclaringTypeName(), cacheKey);
                 return cacheValue;
@@ -64,25 +60,15 @@ public class CustomCacheableWithLockAspect {
 
             Object result = joinPoint.proceed();
             operations.set(cacheKey, result, Duration.ofMinutes(customCacheableWithLock.ttl()));
-            log.info("[{}] 레디스 캐시 저장 확인 - cacheKey=({})",
+            log.info("[{}] 레디스 캐시 저장 완료 - cacheKey=({})",
                     joinPoint.getSignature().getDeclaringTypeName(), cacheKey);
             return result;
 
-        } catch (Exception e){
-            throw GlobalErrorCode.CANNOT_GET_LOCK.toException();
+        } catch (Exception e) {
+            log.error("[{}] 분산락 획득 중 오류 발생", joinPoint.getSignature().getName());
+            throw GlobalErrorCode.INTERNAL_SERVER_ERROR.toException();
+        } finally {
+            lock.unlock();
         }
-    }
-
-    private String generateCacheKey(ProceedingJoinPoint joinPoint, CustomCacheableWithLock customCacheableWithLock) {
-        if (!customCacheableWithLock.key().isEmpty()) {
-            return customCacheableWithLock.cacheName() + "::" + customCacheableWithLock.key();
-        }
-
-        String methodName = joinPoint.getSignature().getName();
-        String params = Arrays.stream(joinPoint.getArgs())
-                .map(arg -> arg == null ? "null" : arg.toString())
-                .collect(Collectors.joining(","));
-
-        return customCacheableWithLock.cacheName() + "::" + methodName + "(" + params + ")";
     }
 }
