3f6de47007f6ee3e261767ee6af4ef2756fa1bdd
chyun7114 <chyun7114@naver.com>
2025-04-29 00:06:50 +0900

    feat: recruitList관련 mapper, 서비스 로직 작성

--- src/main/java/com/dodream/recruit/infrastructure/RecruitApiCaller.java
+++ src/main/java/com/dodream/recruit/infrastructure/RecruitApiCaller.java
@@ -34,6 +34,7 @@ public class RecruitApiCaller {
             throw RecruitException.API_CONNECTION_ERROR.toException();
         }
     }
+
     @CustomCacheableWithLock(cacheName = "recruitDetail", ttl = 60)
     public String recruitDetatilAPiCaller(
             String id
