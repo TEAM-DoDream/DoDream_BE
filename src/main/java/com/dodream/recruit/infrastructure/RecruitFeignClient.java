52144334a2eea3534ff0fbdd969d500fa41b82d5
chyun7114 <chyun7114@naver.com>
2025-04-28 14:51:02 +0900

    feat: 채용정보 FeignClient 생성

---
+++ src/main/java/com/dodream/recruit/infrastructure/RecruitFeignClient.java
@@ -0,0 +1,24 @@
+package com.dodream.recruit.infrastructure;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "recruitClient", url = "${saramin.url}")
+public interface RecruitFeignClient {
+    @GetMapping(value = "${saramin.endpoint}", headers = "Accept=application/json")
+    String getRecruitList(
+            @RequestParam(name = "access-key") String accessKey,
+            @RequestParam(name = "keywords") String keywords,
+            @RequestParam(name = "loc_cd") String locCd,
+            @RequestParam(name = "start") int start,
+            @RequestParam(name = "count") int count,
+            @RequestParam(name = "sort") String sort
+    );
+
+    @GetMapping(value = "${saramin.endpoint}", headers = "Accept=application/json")
+    String getRecruitDetail(
+            @RequestParam(name = "access-key") String accessKey,
+            @RequestParam(name = "id") String id
+    );
+}
