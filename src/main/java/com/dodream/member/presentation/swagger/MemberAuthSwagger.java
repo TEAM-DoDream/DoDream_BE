14d99b55b26b2d12d8aa338054e340716ca936a6
sseongeun <96720963+sseongeun@users.noreply.github.com>
2025-04-30 14:31:49 +0900

    feat : 로그인, 회원가입 test 코드 작성 (#51)

--- src/main/java/com/dodream/member/presentation/swagger/MemberAuthSwagger.java
+++ src/main/java/com/dodream/member/presentation/swagger/MemberAuthSwagger.java
@@ -12,9 +12,7 @@ import com.dodream.member.exception.MemberErrorCode;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
