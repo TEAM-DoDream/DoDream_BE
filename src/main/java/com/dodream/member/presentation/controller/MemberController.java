cd1a3e36abefb39f8136ebfcb4e624a7f73c976d
sseongeun <96720963+sseongeun@users.noreply.github.com>
2025-05-02 17:13:10 +0900

    chore : 멤버 프로필 사진 변경 기능 구현 (#81)

--- src/main/java/com/dodream/member/presentation/controller/MemberController.java
+++ src/main/java/com/dodream/member/presentation/controller/MemberController.java
@@ -1,2 +1,34 @@
-package com.dodream.member.presentation.controller;public class MemberController {
+package com.dodream.member.presentation.controller;
+
+import com.dodream.core.presentation.RestResponse;
+import com.dodream.member.application.MemberService;
+import com.dodream.member.dto.response.UploadMemberProfileImageResponseDto;
+import com.dodream.member.presentation.swagger.MemberSwagger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@RestController
+@RequestMapping("/v1/member")
+@RequiredArgsConstructor
+@Validated
+public class MemberController implements MemberSwagger {
+
+    private final MemberService memberService;
+
+    @Override
+    @PostMapping(value = "/profile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<RestResponse<UploadMemberProfileImageResponseDto>> uploadMemberProfileImage(
+        @RequestParam("file") MultipartFile file) {
+        return ResponseEntity.ok(
+            new RestResponse<>(memberService.uploadMemberProfileImage(file)));
+    }
+
 }
--- src/main/java/com/dodream/member/presentation/swagger/MemberSwagger.java
+++ src/main/java/com/dodream/member/presentation/swagger/MemberSwagger.java
@@ -1,2 +1,26 @@
-package com.dodream.member.presentation.swagger;public interface MemberSwagger {
+package com.dodream.member.presentation.swagger;
+
+import com.dodream.core.config.swagger.ApiErrorCode;
+import com.dodream.core.presentation.RestResponse;
+import com.dodream.member.dto.response.UploadMemberProfileImageResponseDto;
+import com.dodream.member.exception.MemberErrorCode;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Tag(name = "My account", description = "나의 계정 관련 API")
+public interface MemberSwagger {
+
+    @Operation(
+        summary = "회원 프로필 사진 등록 및 변경 API",
+        description = "사진을 등록하여 프로필 사진을 등록 및 변경한다.",
+        operationId = "/v1/member/profile"
+    )
+    @ApiErrorCode(MemberErrorCode.class)
+    ResponseEntity<RestResponse<UploadMemberProfileImageResponseDto>> uploadMemberProfileImage(
+        @RequestParam("file") MultipartFile file);
+
+
 }
