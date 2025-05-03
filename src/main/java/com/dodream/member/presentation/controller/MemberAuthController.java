a935ee89682bebee726dbd8a2d94cb8e7abb01df
sseongeun <96720963+sseongeun@users.noreply.github.com>
2025-05-03 13:54:25 +0900

    fix : annotation 정리 (#80)

--- src/main/java/com/dodream/member/presentation/controller/MemberAuthController.java
+++ src/main/java/com/dodream/member/presentation/controller/MemberAuthController.java
@@ -16,12 +16,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/member/auth")
