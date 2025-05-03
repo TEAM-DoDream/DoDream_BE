86ea4bdcf738966c7a8d696aef12b6f030e6ccb3
sseongeun <96720963+sseongeun@users.noreply.github.com>
2025-05-03 20:35:04 +0900

    refactor : 닉네임 포함하여 반환 (#92)

--- src/main/java/com/dodream/member/dto/response/MemberLoginResponseDto.java
+++ src/main/java/com/dodream/member/dto/response/MemberLoginResponseDto.java
@@ -4,12 +4,14 @@ import com.dodream.ncs.domain.Ncs;
 
 public record MemberLoginResponseDto(
     String memberId,
+    String nickname,
     String accessToken,
     String refreshToken
 ) {
 
-    public static MemberLoginResponseDto of(String memberId, String accessToken, String refreshToken) {
-        return new MemberLoginResponseDto(memberId,accessToken,refreshToken);
+    public static MemberLoginResponseDto of(String memberId, String nickname, String accessToken,
+        String refreshToken) {
+        return new MemberLoginResponseDto(memberId, nickname, accessToken, refreshToken);
     }
 
 }
