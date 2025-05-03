f9e7966a20faf14a692ac74993951c420eba241f
chyun7114 <chyun7114@naver.com>
2025-05-03 16:01:49 +0900

    feat: job 엔티티 생성

---
+++ src/main/java/com/dodream/job/domain/Level.java
@@ -0,0 +1,7 @@
+package com.dodream.job.domain;
+
+public enum Level {
+    HIGH,
+    MEDIUM,
+    LOW
+}
---
+++ src/main/java/com/dodream/job/domain/Require.java
@@ -0,0 +1,7 @@
+package com.dodream.job.domain;
+
+public enum Require {
+    NONE,
+    OPTIONAL,
+    REQUIRED
+}
