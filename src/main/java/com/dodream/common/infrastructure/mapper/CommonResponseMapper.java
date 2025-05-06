8d60036055bea921340fd1bd971b2cc54ea05dff
chyun7114 <chyun7114@naver.com>
2025-05-06 17:34:24 +0900

    refactor: mapper클래스 모두 abstract 클래스로 변경

--- src/main/java/com/dodream/common/infrastructure/mapper/CommonResponseMapper.java
+++ src/main/java/com/dodream/common/infrastructure/mapper/CommonResponseMapper.java
@@ -3,24 +3,24 @@ package com.dodream.common.infrastructure.mapper;
 import com.dodream.common.dto.response.CommonResponse;
 import com.dodream.common.exception.CommonErrorCode;
 import com.dodream.core.infrastructure.converter.XmlToJsonConverter;
+import com.dodream.core.infrastructure.mapper.AbstractObjectMapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
-public class CommonResponseMapper {
+public class CommonResponseMapper extends AbstractObjectMapper<CommonResponse> {
 
     private final XmlToJsonConverter xmlToJsonConverter;
-    private final ObjectMapper objectMapper;
+
+    public CommonResponseMapper(ObjectMapper objectMapper, XmlToJsonConverter xmlToJsonConverter) {
+        super(objectMapper);
+        this.xmlToJsonConverter = xmlToJsonConverter;
+    }
 
     public CommonResponse toCommonResponse(String xml) {
         String json = xmlToJsonConverter.convertXmlToJson(xml);
-        try {
-            return objectMapper.readValue(json, CommonResponse.class);
-        } catch (JsonProcessingException e) {
-            throw CommonErrorCode.JSON_TO_OBJECT_CONVERT_ERROR.toException();
-        }
+        return parse(json, CommonResponse.class);
     }
 }
--- src/main/java/com/dodream/training/infrastructure/TrainingContextConfig.java
+++ src/main/java/com/dodream/training/infrastructure/TrainingContextConfig.java
@@ -2,9 +2,13 @@ package com.dodream.training.infrastructure;
 
 import com.dodream.training.infrastructure.caller.BootCampApiCaller;
 import com.dodream.training.infrastructure.caller.DualTrainingApiCaller;
+import com.dodream.training.infrastructure.mapper.TrainingDetailResponseDtoMapper;
+import com.dodream.training.infrastructure.mapper.TrainingListApiReseponseMapper;
+import com.dodream.training.infrastructure.mapper.TrainingMapper;
 import com.dodream.training.util.executer.BootcampApiExecuter;
 import com.dodream.training.util.executer.DualTrainingApiExecuter;
 import com.dodream.training.util.executer.TrainingApiExecuter;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +20,7 @@ public class TrainingContextConfig {
     private final BootCampApiCaller bootCampApiCaller;
     private final DualTrainingApiCaller dualTrainingApiCaller;
     private final TrainingDatePolicy trainingDatePolicy;
+    private final ObjectMapper objectMapper;
 
     @Bean
     public TrainingApiExecuter bootcampApiExecuter() {
@@ -25,5 +30,15 @@ public class TrainingContextConfig {
     @Bean
     public TrainingApiExecuter dualTrainingApiExecuter() {
         return new DualTrainingApiExecuter(dualTrainingApiCaller, trainingDatePolicy);
+    }
+
+    @Bean
+    public TrainingMapper trainingListApiReseponseMapper(){
+        return new TrainingListApiReseponseMapper(objectMapper);
+    }
+
+    @Bean
+    public TrainingMapper trainingDetailResponseDtoMapper(){
+        return new TrainingDetailResponseDtoMapper(objectMapper);
     }
 }
