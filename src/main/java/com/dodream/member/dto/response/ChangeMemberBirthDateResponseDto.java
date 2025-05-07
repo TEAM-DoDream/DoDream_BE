fe926eb909560b534b7e15706eb4b38dcd9789c8
sseongeun <96720963+sseongeun@users.noreply.github.com>
2025-05-07 14:42:31 +0900

    refactor : 생년월일 jsonformat형식 변경 (#112)

--- src/main/java/com/dodream/member/dto/response/ChangeMemberBirthDateResponseDto.java
+++ src/main/java/com/dodream/member/dto/response/ChangeMemberBirthDateResponseDto.java
@@ -7,8 +7,8 @@ import java.time.LocalDate;
 public record ChangeMemberBirthDateResponseDto(
     @Schema(description = "멤버 id(DB상)", example = "1")
     Long memberId,
-    @Schema(description = "새로운 생년월일", example = "2001-01-05", type = "string")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @Schema(description = "새로운 생년월일", example = "2001/01/05", type = "string")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy/MM/dd")
     LocalDate newBirthDate,
     @Schema(description = "메세지", example = "생년월일 변경 완료")
     String message
