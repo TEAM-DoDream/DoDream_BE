682469fe8b98dd547864d5a3b49f8fcd499109d0
chyun7114 <chyun7114@naver.com>
2025-05-11 10:53:54 +0900

    fix: customUserDetails null일때 오류 핸들링 강화

--- src/main/java/com/dodream/job/exception/JobErrorCode.java
+++ src/main/java/com/dodream/job/exception/JobErrorCode.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum JobErrorCode implements BaseErrorCode<DomainException> {
     CANNOT_CONVERT_JOB_OBJECT(HttpStatus.INTERNAL_SERVER_ERROR, "직무정보 객체로의 변환에 실패했습니다."),
+    CANNOT_FIND_USER_DATA(HttpStatus.NOT_FOUND, "온보딩을 진행한 유저 정보를 찾을 수 없습니다."),
     CANNOT_GET_CLOVA_RESPONSE(HttpStatus.INTERNAL_SERVER_ERROR, "Clova Studio 응답을 받지 못했습니다."),
     CANNOT_CONVERT_CLOVA_RESPONSE(HttpStatus.INTERNAL_SERVER_ERROR, "객체 변환에 실패했습니다."),
     CANNOT_GET_JOB_DATA(HttpStatus.NOT_FOUND, "직업 데이터를 찾을 수 없습니다."),
