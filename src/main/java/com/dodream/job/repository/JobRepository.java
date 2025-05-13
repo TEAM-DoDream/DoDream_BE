e4a3b7bc2325ca395ada772c0f72d9b478848478
chyun7114 <chyun7114@naver.com>
2025-05-13 14:19:52 +0900

    feat: 키워드 없이 검색할 때 투두 그룹 별로 키워드 잡도록 수정

--- src/main/java/com/dodream/job/repository/JobRepository.java
+++ src/main/java/com/dodream/job/repository/JobRepository.java
@@ -2,9 +2,23 @@ package com.dodream.job.repository;
 
 import com.dodream.job.domain.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface JobRepository extends JpaRepository<Job, Long> {
     Optional<Job> findByJobName(String jobName);
+
+    @Query(value = """
+        SELECT j.*
+        FROM job j
+        JOIN (
+            SELECT job_id
+            FROM todo_group
+            GROUP BY job_id
+            ORDER BY COUNT(*) DESC
+            LIMIT 1
+        ) AS top_job ON j.id = top_job.job_id
+    """, nativeQuery = true)
+    Optional<Job> findMostPopularJob();
 }
