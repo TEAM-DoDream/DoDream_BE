b6351f1dcea57822707c072a0e6843313a461750
sseongeun <96720963+sseongeun@users.noreply.github.com>
2025-05-15 20:26:07 +0900

    feat : 타유저 투두 리스트 조회 api 구현 (#158)

--- src/main/java/com/dodream/todo/repository/TodoGroupRepository.java
+++ src/main/java/com/dodream/todo/repository/TodoGroupRepository.java
@@ -5,6 +5,7 @@ import com.dodream.member.domain.Member;
 import com.dodream.todo.domain.TodoGroup;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,6 +14,8 @@ import org.springframework.data.repository.query.Param;
 public interface TodoGroupRepository extends JpaRepository<TodoGroup, Long> {
 
     Optional<TodoGroup> findByIdAndMember(Long id, Member member);
+
+    Optional<TodoGroup> findByIdAndMemberNot(Long id, Member member);
 
     List<TodoGroup> findAllByMember(Member member);
 
@@ -23,10 +26,21 @@ public interface TodoGroupRepository extends JpaRepository<TodoGroup, Long> {
     boolean existsByMemberAndJob(Member member, Job job);
 
     @Query("SELECT tg FROM TodoGroup tg " +
-           "WHERE tg.member <> :currentMember " +
-           "AND tg.job = :job " +
-           "AND tg.deleted = false")
-    List<TodoGroup> findTop3ByJobAndNotMember(@Param("job") Job job, @Param("currentMember") Member currentMember, Pageable pageable);
+        "WHERE tg.member <> :currentMember " +
+        "AND tg.job = :job " +
+        "AND tg.deleted = false")
+    List<TodoGroup> findTop3ByJobAndNotMember(@Param("job") Job job,
+        @Param("currentMember") Member currentMember, Pageable pageable);
+
+    @Query("SELECT tg FROM TodoGroup tg " +
+        "WHERE tg.member <> :currentMember " +
+        "AND tg.job = :job " +
+        "AND tg.deleted = false")
+    Page<TodoGroup> findTop10ByJobAndNotMember(@Param("job") Job job,
+        @Param("currentMember") Member currentMember, Pageable pageable);
 
     List<TodoGroup> findTop3ByMemberOrderByTotalViewDesc(Member member);
+
+    List<TodoGroup> findTop5ByJobAndMemberNot(Job job, Member member);
+
 }
