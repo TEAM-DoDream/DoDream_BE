3b7325a8405e5f4d29336c35f992624d531e9ced
chyun7114 <chyun7114@naver.com>
2025-05-15 18:17:21 +0900

    feat: 훈련 과정 찾기 api를 필터에 맞춰서 제작

--- src/main/java/com/dodream/training/controller/TrainingController.java
+++ src/main/java/com/dodream/training/controller/TrainingController.java
@@ -23,53 +23,41 @@ public class TrainingController implements TrainingSwagger {
     private final BootcampService bootcampService;
     private final DualTrainingService dualTrainingService;
 
-    @GetMapping("/bootcamp/list")
+    @GetMapping("/list")
     public ResponseEntity<RestResponse<TrainingListApiResponse>> getBootcampList(
         @RequestParam String pageNum,
+        @RequestParam(required = false) String type,
         @RequestParam(required = false) String regionName,
         @RequestParam(required = false) String jobName,
         @RequestParam(required = false) LocalDate startDate,
         @RequestParam(required = false) LocalDate endDate
     ){
-        return ResponseEntity.ok(new RestResponse<>(
-                bootcampService.getList(pageNum, regionName, jobName, startDate, endDate))
-        );
+        if(type.equals("이론 위주")){
+            return ResponseEntity.ok(new RestResponse<>(
+                    bootcampService.getList(pageNum, regionName, jobName, startDate, endDate)
+            ));
+        }else{
+            return ResponseEntity.ok(new RestResponse<>(
+                    dualTrainingService.getList(pageNum, regionName, jobName, startDate, endDate))
+            );
+        }
     }
 
-    @GetMapping("/bootcamp/detail")
+    @GetMapping("/detail")
     public ResponseEntity<RestResponse<TrainingDetailApiResponse>> getBootcampDetail(
+            @RequestParam(required = false) String type,
             @RequestParam String srchTrprId,
             @RequestParam String srchTrprDegr,
             @RequestParam String srchTorgId
     ){
-        return ResponseEntity.ok(new RestResponse<>(
-                bootcampService.getDetail(srchTrprId, srchTrprDegr, srchTorgId)
-        ));
-    }
-
-    @GetMapping("/dual/list")
-    @Override
-    public ResponseEntity<RestResponse<TrainingListApiResponse>> getDualTrainingList(
-            @RequestParam String pageNum,
-            @RequestParam(required = false) String regionName,
-            @RequestParam(required = false) String jobName,
-            @RequestParam(required = false) LocalDate startDate,
-            @RequestParam(required = false) LocalDate endDate
-    ) {
-        return ResponseEntity.ok(new RestResponse<>(
-                dualTrainingService.getList(pageNum, regionName, jobName, startDate, endDate)
-        ));
-    }
-
-    @GetMapping("/dual/detail")
-    @Override
-    public ResponseEntity<RestResponse<TrainingDetailApiResponse>> getDualTrainingDetail(
-            @RequestParam String srchTrprId,
-            @RequestParam String srchTrprDegr,
-            @RequestParam String srchTorgId
-    ) {
-        return ResponseEntity.ok(new RestResponse<>(
-                dualTrainingService.getDetail(srchTrprId, srchTrprDegr, srchTorgId)
-        ));
+        if(type.equals("이론 위주")){
+            return ResponseEntity.ok(new RestResponse<>(
+                    bootcampService.getDetail(srchTrprId, srchTrprDegr, srchTorgId)
+            ));
+        }else{
+            return ResponseEntity.ok(new RestResponse<>(
+                    dualTrainingService.getDetail(srchTrprId, srchTrprDegr, srchTorgId)
+            ));
+        }
     }
 }
