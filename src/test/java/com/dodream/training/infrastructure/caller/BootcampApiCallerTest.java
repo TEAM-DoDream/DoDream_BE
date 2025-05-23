96875a5bdc7d6429f6ba2d6c3baa4ce4d168ed2f
chyun7114 <chyun7114@naver.com>
2025-05-23 13:57:45 +0900

    feat: caller 관련 단위 테스트 작성

--- src/test/java/com/dodream/training/infrastructure/caller/BootcampApiCallerTest.java
+++ src/test/java/com/dodream/training/infrastructure/caller/BootcampApiCallerTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import static org.mockito.BDDMockito.*;
@@ -28,6 +27,10 @@ public class BootcampApiCallerTest {
     private final String PAGE_NUM = "1";
     private final SortBy SORT_BY = SortBy.DEADLINE_DESC;
 
+    private final String TRPR_ID = "A12345678";
+    private final String TRPR_DEG = "3";
+    private final String TORG_ID = "4567895456";
+
     @BeforeEach
     void setUp(){
         ReflectionTestUtils.setField(bootcampApiCaller, "apiKey", "dummy-api-key");
@@ -36,7 +39,7 @@ public class BootcampApiCallerTest {
 
     @Nested
     @DisplayName("[getListApi] 리스트 api 테스트")
-    class getListApi{
+    class GetListTest{
 
         @Test
         @DisplayName("성공 테스트")
@@ -67,5 +70,41 @@ public class BootcampApiCallerTest {
                     .isInstanceOf(DomainException.class)
                     .hasMessageContaining("외부 API 서버에서 값을 불러오는데 실패하였습니다.");
         }
+    }
+
+    @Nested
+    @DisplayName("[getDetailApi] 세부 정보 api 테스트")
+    class GetDetailApiTest{
+
+        @Test
+        @DisplayName("세부 정보 반환 성공")
+        void getDetailApi_success(){
+            // given
+            given(bootcampFeignClient.searchBootCampDetail(
+                    any(), any(), any(), any(), any(), any()
+            )).willReturn("response");
+
+            // when
+            String result
+                    = bootcampApiCaller.getDetailApi(TRPR_ID, TRPR_DEG, TORG_ID);
+
+            // then
+            assertThat(result).isEqualTo("response");
+        }
+
+        @Test
+        @DisplayName("세부 정보 반환 실패")
+        void getDetailApi_fail(){
+            // given
+            given(bootcampFeignClient.searchBootCampDetail(
+                    any(), any(), any(), any(), any(), any()
+            )).willThrow(DomainException.class);
+
+            // when & then
+            assertThatThrownBy(() -> bootcampApiCaller.getDetailApi(TRPR_ID, TRPR_DEG, TORG_ID))
+                    .isInstanceOf(DomainException.class)
+                    .hasMessageContaining("외부 API 서버에서 값을 불러오는데 실패하였습니다.");
+        }
+
     }
 }
