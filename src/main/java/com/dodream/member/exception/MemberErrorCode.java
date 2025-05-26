7b0b48c120e53b11b17273fb320f6359bab54993
sseongeun <96720963+sseongeun@users.noreply.github.com>
2025-05-26 15:11:48 +0900

    refactor : 에러 메세지 수정

--- src/main/java/com/dodream/member/exception/MemberErrorCode.java
+++ src/main/java/com/dodream/member/exception/MemberErrorCode.java
@@ -14,7 +14,7 @@ public enum MemberErrorCode implements BaseErrorCode<DomainException> {
     DUPLICATE_MEMBER_ID(HttpStatus.CONFLICT,"이미 사용 중인 아이디입니다."),
     DUPLICATE_NICKNAME(HttpStatus.CONFLICT ,"이미 사용 중인 닉네임입니다."),
     MEMBER_INACTIVE(HttpStatus.BAD_REQUEST, "비활성화된 계정입니다."),
-    PASSWORD_NOT_SAME(HttpStatus.BAD_REQUEST, "두 비밀번호가 일치하지 않습니다.."),
+    PASSWORD_NOT_SAME(HttpStatus.BAD_REQUEST, "두 비밀번호가 일치하지 않습니다."),
     UNSUPPORTED_FILE_EXTENSION(HttpStatus.BAD_REQUEST, "지원하지 않는 파일 확장자입니다.(jpg,jpeg,png만 허용)"),
     FILE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드 중 문제가 발생했습니다."),
     FILE_DELETE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "파일 삭제 중 문제가 발생했습니다.");
