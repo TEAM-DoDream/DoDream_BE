6da3d6f69c5ae8de1ce575828096f7f914bb65a1
chyun7114 <chyun7114@naver.com>
2025-05-27 10:51:43 +0900

    refactor: dto parameter 통일

--- src/main/java/com/dodream/scrap/dto/response/TrainingScrapResponseDto.java
+++ src/main/java/com/dodream/scrap/dto/response/TrainingScrapResponseDto.java
@@ -3,6 +3,7 @@ package com.dodream.scrap.dto.response;
 import com.dodream.scrap.domain.entity.MemberTrainingScrap;
 import com.dodream.training.util.TrainingDateUtils;
 import com.dodream.training.util.TrainingPriceUtils;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record TrainingScrapResponseDto(
@@ -10,26 +11,37 @@ public record TrainingScrapResponseDto(
         @Schema(description = "스크랩 id", example = "1")
         Long scrapId,
 
+        @JsonProperty("trprId")
         @Schema(description = "훈련 과정 id", example = "AIG20240000492114")
         String trainingId,
 
+        @JsonProperty("title")
         @Schema(description = "훈련 과정 이름", example = "요양보호사자격증취득과정 (사회복지사)")
         String trainingName,
 
+        @JsonProperty("subTitle")
         @Schema(description = "훈련 기관 이름", example = "고색요양보호사교육원")
         String trainingOrgName,
 
+        @JsonProperty("address")
         @Schema(description = "훈련 기관 주소", example = "경기 수원시 권선구")
         String trainingOrgAddr,
 
+        @JsonProperty("trprDegr")
         @Schema(description = "훈련과정 차수", example = "17")
         String trainingDegree,
 
+        @JsonProperty("traDuration")
         @Schema(description = "훈련 과정 기간", example = "약 3개월")
         String trainingDuration,
 
+        @JsonProperty("realMan")
         @Schema(description = "훈련 과정 가격", example = "1000000")
-        String trainingPrice
+        String trainingPrice,
+
+        @JsonProperty("titleLink")
+        @Schema(description = "훈련과정 url", example = "http://~")
+        String titleLink
 ) {
 
     public static TrainingScrapResponseDto from(MemberTrainingScrap memberTrainingScrap) {
@@ -44,7 +56,8 @@ public record TrainingScrapResponseDto(
                             memberTrainingScrap.getTrainingStartDate(),
                             memberTrainingScrap.getTrainingEndDate()
                     ),
-                    TrainingPriceUtils.convertDecimalFormatForPrice(memberTrainingScrap.getTrainingManage())
+                    TrainingPriceUtils.convertDecimalFormatForPrice(memberTrainingScrap.getTrainingManage()),
+                    memberTrainingScrap.getTrainingUrl()
             );
     }
 }
