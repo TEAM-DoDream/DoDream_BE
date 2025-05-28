1138aa8946eac32ff60d68954ceb3911308165a2
chyun7114 <chyun7114@naver.com>
2025-05-28 15:05:54 +0900

    fix: StringBuilder관련 thread안전성 보장

--- src/main/java/com/dodream/training/util/TrainingUrlUtils.java
+++ src/main/java/com/dodream/training/util/TrainingUrlUtils.java
@@ -2,15 +2,12 @@ package com.dodream.training.util;
 
 public class TrainingUrlUtils {
 
-    private static String BASE_URL = "https://hrd.work24.go.kr/hrdp/co/pcobo/PCOBO0100P.do?";
-
-    private static StringBuilder sb = new StringBuilder();
+    private static final String BASE_URL = "https://hrd.work24.go.kr/hrdp/co/pcobo/PCOBO0100P.do?";
 
     public static String generateTrainingUrl(String trprId, String trprDeg, String torgId){
-        return sb.append(BASE_URL)
-                .append("tracseId=").append(trprId)
-                .append("&tracseTme=").append(trprDeg)
-                .append("&trainstCstmrId=").append(torgId)
-                .toString();
+        return BASE_URL +
+                "tracseId=" + trprId +
+                "&tracseTme=" + trprDeg +
+                "&trainstCstmrId=" + torgId;
     }
 }
