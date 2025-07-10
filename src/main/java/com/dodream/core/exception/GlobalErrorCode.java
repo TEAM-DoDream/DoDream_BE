d0bd9605dd94a34fc93fde058dc426a18c58aa53
chyun7114 <chyun7114@naver.com>
2025-07-10 20:13:21 +0900

    Feat: EmailSenderUtil 생성

--- src/main/java/com/dodream/core/exception/GlobalErrorCode.java
+++ src/main/java/com/dodream/core/exception/GlobalErrorCode.java
@@ -18,7 +18,8 @@ public enum GlobalErrorCode implements BaseErrorCode<RuntimeException> {
     DB_CONNECTION_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB 연결에 실패했습니다."),
     ALREADY_PROCESS_STARTED(HttpStatus.BAD_REQUEST, "이미 처리중인 요청입니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 내부 오류입니다."),
-    NOT_CONVERT_JSON_TO_OBJECT(HttpStatus.INTERNAL_SERVER_ERROR, "JSON -> Object 변환을 실패했습니다.");
+    NOT_CONVERT_JSON_TO_OBJECT(HttpStatus.INTERNAL_SERVER_ERROR, "JSON -> Object 변환을 실패했습니다."),
+    CANNOT_SEND_MAIL(HttpStatus.INTERNAL_SERVER_ERROR, "이메일을 전송하는데 실패했습니다.");
 
     private final HttpStatus httpStatus;
 
