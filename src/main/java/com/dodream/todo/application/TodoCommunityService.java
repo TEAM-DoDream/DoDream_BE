73e878c383552eadd0a96be72ec8bc782420922c
chyun7114 <chyun7114@naver.com>
2025-08-23 23:24:57 +0900

    Feat: 무한스크롤 투두 조회 구현

--- src/main/java/com/dodream/todo/application/TodoCommunityService.java
+++ src/main/java/com/dodream/todo/application/TodoCommunityService.java
@@ -1,13 +1,17 @@
 package com.dodream.todo.application;
 
+import com.dodream.member.domain.Level;
 import com.dodream.todo.dto.response.TodoCommunityResponse;
 import com.dodream.todo.dto.response.TodoCommunityResponseDto;
 import com.dodream.todo.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
 
 @Service
 @RequiredArgsConstructor
@@ -29,4 +33,25 @@ public class TodoCommunityService {
     }
 
     // 투두 필터에 따른 투두 목록을 출력한다(무한 스크롤)
+    public Slice<TodoCommunityResponseDto> searchTodoList(String jobName, String level, String sort, int page, int size) {
+
+        Pageable pageable = PageRequest.of(page, size);
+
+        Slice<TodoCommunityResponse> slice = todoRepository.findTodosWithSlice(jobName, getLevel(level), sort, pageable);
+
+        List<TodoCommunityResponseDto> content = slice.stream()
+                .map(TodoCommunityResponseDto::of)
+                .toList();
+
+        return new SliceImpl<>(content, pageable, slice.hasNext());
+    }
+
+    private Level getLevel(String level) {
+        return switch (level) {
+            case "1단계: 씨앗" -> Level.SEED;
+            case "2단계: 새싹" -> Level.SPROUT;
+            case "3단계: 꿈나무" -> Level.TREE;
+            default -> null;
+        };
+    }
 }
--- src/main/java/com/dodream/todo/repository/TodoRepositoryCustom.java
+++ src/main/java/com/dodream/todo/repository/TodoRepositoryCustom.java
@@ -1,9 +1,16 @@
 package com.dodream.todo.repository;
 
+import com.dodream.member.domain.Level;
 import com.dodream.todo.dto.response.TodoCommunityResponse;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 import java.util.List;
 
 public interface TodoRepositoryCustom {
+
     List<TodoCommunityResponse> findByTop5SaveTodo(String jobName);
+
+    Slice<TodoCommunityResponse> findTodosWithSlice(String jobName, Level level, String sort, Pageable pageable);
 }
