6d7f118ee12c7c56430c5de5ed53740ce3d92e2b
sseongeun <96720963+sseongeun@users.noreply.github.com>
2025-09-04 16:55:51 +0900

    feat : 타유저 레벨도 포함하여 반환하도록 수정 (#269)

--- src/main/java/com/dodream/todo/dto/response/GetOthersTodoGroupResponseDto.java
+++ src/main/java/com/dodream/todo/dto/response/GetOthersTodoGroupResponseDto.java
@@ -19,6 +19,8 @@ public record GetOthersTodoGroupResponseDto(
     String profileImage,
     @Schema(description = "거주지", example = "대전 광역시 서구")
     String regionName,
+    @Schema(description = "멤버 단계", example = "새싹 단계")
+    String memberLevel,
     @Schema(description = "투두 경과 일자(X일)", example = "38")
     Long daysAgo,
     @Schema(description = "직업 이름", example = "요양보호사")
@@ -37,6 +39,8 @@ public record GetOthersTodoGroupResponseDto(
             .memberNickname(todoGroup.getMember().getNickName())
             .profileImage(todoGroup.getMember().getProfileImage())
             .regionName(todoGroup.getMember().getRegion().getRegionName())
+            .memberLevel(
+                todoGroup.getMember().getLevel() == null ? null : todoGroup.getMember().getLevel().getValue())
             .daysAgo(
                 ChronoUnit.DAYS.between(todoGroup.getCreatedAt().toLocalDate(), LocalDate.now())
                     + 1)
